3c8b9869e5368780dab7f8e319a3f0bb4e5cdf9d
renan <renan.eiras.melo@gmail.com>
2021-03-08 19:57:53 -0300

    chore: env

--- src/app/server.js
+++ src/app/server.js
@@ -1,6 +1,3 @@
 const app = require('./main');
 
-const { PORT } = process.env;
-app.listen(PORT, () =>
-  console.log(`✔ server is running at http://localhost:${PORT}/`),
-);
+app.listen(process.env.PORT || 3333);
